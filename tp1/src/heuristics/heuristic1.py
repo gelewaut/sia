19e92a3ce8c39f3b1b0377f06fcc1fa0777c8e83
juampiarias <juaarias@itba.edu.ar>
2023-08-21 16:56:16 -0300

    heuristic1 done

--- tp1/src/heuristics/heuristic1.py
+++ tp1/src/heuristics/heuristic1.py
@@ -17,8 +17,7 @@ def find_closest_box_bfs(board, size):
     while len(Fr) != 0:
         aux = Fr.pop(0)
         if agent_next_to_box(aux.getBoard(), sok.find_agent(aux.getBoard(), size)):
-            distance += 1
-            return distance
+            return aux.cost
         y,x = sok.find_agent(aux.getBoard(), size)
 
         append_fronteer_bfs(Fr, Exp, aux, size, x,y, sok.UP)
@@ -31,13 +30,12 @@ def find_closest_box_bfs(board, size):
 def append_fronteer_bfs(fronteer, explored, current_node, size, x, y, direction):
     board_aux,movement = sok.move_agent(current_node.getBoard(), x, y, direction)
     if movement and sok.can_continue(board_aux, sok.find_boxes(board_aux, size)):
-        distance += 1
-        node = Node(board_aux, current_node, 0)
+        node = Node(board_aux, current_node, current_node.cost + 1)
         if node not in explored:
             fronteer.append (node)
             
 
-def agent_next_to_box(agent, board):
+def agent_next_to_box(board, agent):
     x = agent[1]
     y = agent[0]
     if board[y+1][x] == sok.BOX or board[y-1][x] == sok.BOX or board[y][x+1] == sok.BOX or board[y][x-1] == sok.BOX:
