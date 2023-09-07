84b1e633586cdd531e1e7aa15d0aae0d69c98403
Francisco Dominguez <fdominguez@itba.edu.ar>
2023-09-07 12:41:33 -0300

    Roulette, universal and elite selection done

--- tp2/src/selection.py
+++ tp2/src/selection.py
@@ -1,8 +1,9 @@
 import numpy
 import math
+import random
 
 
-def __population_fitness__ (generation):
+def population_fitness(generation):
     population_fitnesses = []
     total_fitness = 0
 
@@ -12,16 +13,28 @@ def __population_fitness__ (generation):
     
     return population_fitnesses, total_fitness
 
+def accumulated_population_fitness(generation):
+    population_fitness, total_fitness = population_fitness(generation)
+    accumulated_fitnesses = []
+    relative_fitness = []
+    sum_accumulated_fitnesses = 0
 
-def __elite_selection__(generation, new_generation, max_population):
-    gen = generation + new_generation
+    for i in range(len(population_fitness)):        
+        relative_fitness[i] += (population_fitness[i]/total_fitness)
+        sum_accumulated_fitnesses += relative_fitness[i]
+        accumulated_fitnesses[i] += sum_accumulated_fitnesses
+    
+    return accumulated_fitnesses,relative_fitness, sum_accumulated_fitnesses
 
-    if (len(gen) <= max_population):
-        return gen
+def elite_selection(population, max_population):
+    
 
-    population_fitnesses, total_fitness = __population_fitness__(gen)
+    if (len(population) <= max_population):
+        return population
 
-    array = [(gen[i], population_fitnesses[i]) for i in range(len(gen))]
+    population_fitnesses, total_fitness = population_fitness(population)
+
+    array = [(population[i], population_fitnesses[i]) for i in range(len(population))]
     array.sort(key=lambda x:x[1])
 
     next_gen = []
@@ -32,6 +45,47 @@ def __elite_selection__(generation, new_generation, max_population):
             aux += 1
 
     return next_gen
+
+def roulette_selection(population, max_population):
+    next_gen = []
+    accumulated_fitnesses,relative_fitness, sum_accumulated_fitnesses = accumulated_population_fitness(population)
+    chosen = [False] * len(population)
+
+
+    while (len(next_gen) < max_population):
+        random_number = random.random()
+
+        for i in range(len(population)):
+            if(random_number <= accumulated_fitnesses[i] and not chosen[i]):
+                next_gen.append(population[i])
+                chosen[i] = True
+                break
+            
+    return next_gen
+
+#Chequear si funciona bien este
+def universal_selection(population, max_population):
+    next_gen = []
+    accumulated_fitnesses,relative_fitness, sum_accumulated_fitnesses = accumulated_population_fitness(population)
+    chosen = [False] * len(population)
+
+
+    while (len(next_gen) < max_population):
+        r = random.random()
+        random_number = (r+len(next_gen))/max_population
+        for i in range(len(population)):
+            if(random_number <= accumulated_fitnesses[i] and not chosen[i]):
+                next_gen.append(population[i])
+                chosen[i] = True
+                break
+            
+    return next_gen
+
+# def ranking_selection(population, max_population):
+
+
+
+
 
 
 
