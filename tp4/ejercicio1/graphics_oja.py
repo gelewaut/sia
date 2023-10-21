87caa241ba067d231cfa0f14308e65bc7524d543
Facundo Anselmi <anselfac@fit.cvut.cz>
2023-10-21 17:17:16 -0300

    Cambios Oja

--- tp4/ejercicio1/graphics_oja.py
+++ tp4/ejercicio1/graphics_oja.py
@@ -1,3 +1,10 @@
+import configparser
+
+import numpy
+from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.decomposition import PCA
+
 from oja_pca import OjaPCA
 import pandas as pd
 import numpy as np
@@ -7,20 +14,46 @@ if __name__ == "__main__":
     entry = pd.read_csv("data/europe.csv").values
     countries = entry[:, 0]
     data = entry[:, 1:].astype(float)
-    X = np.array(data)
+    data = stats.zscore(data, axis=0)
 
-    learning_rates = [0.1, 0.01, 0.001]
-    iterations = [1000, 10000, 50000]
-    results = []
-    for learning_rate in learning_rates:
-        for iteration in iterations:
-            oja_pca = OjaPCA(learning_rate, iteration, True)
-            oja_pca.fit(X)
-            first_principal_component = oja_pca.get_first_principal_component()
-            results.append([learning_rate, iteration, True, first_principal_component])
-            print(first_principal_component)
-            oja_pca = OjaPCA(learning_rate, iteration, False)
-            oja_pca.fit(X)
-            first_principal_component = oja_pca.get_first_principal_component()
-            results.append([learning_rate, iteration, False, first_principal_component])
-            print(first_principal_component)
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    eta = config.getfloat('Oja', 'eta')
+    decreasing_eta = config.getboolean('Oja', 'decreasing_eta')
+    epochs = config.getint('Oja', 'epochs')
+
+    oja = OjaPCA(eta, decreasing_eta, data)
+    # oja.fit(epochs)
+    # fpc = oja.get_first_principal_component()
+    # print(fpc)
+
+    pca = PCA()
+    pca.fit(data)
+    print("PC1 Vector:", pca.components_[0])
+
+    distances = []
+    epochs_axis = []
+    for i in range(10):
+        oja.fit(epochs)
+        fpc = oja.get_first_principal_component()
+        print(fpc)
+        distances.append(numpy.linalg.norm(pca.components_[0] - fpc))
+        epochs_axis.append((i+1)*epochs)
+
+    plt.xlabel('Epocas')
+    plt.ylabel('Distancia')
+    plt.plot(epochs_axis, distances, color='red', linestyle='-')
+    plt.show()
+
+    # index = []
+    # for x in X_normalized:
+    #     index.append(fpc.dot(x))
+    #
+    # plt.figure(figsize=(15, 10))
+    # plt.bar(countries, index)
+    # plt.xlabel('Countries')
+    # plt.ylabel('Values')
+    # plt.xticks(rotation=45)
+    # plt.show()
+
+
--- tp4/ejercicio1/oja_pca.py
+++ tp4/ejercicio1/oja_pca.py
@@ -1,53 +1,22 @@
 import numpy as np
-import pandas as pd
 
 class OjaPCA:
-    def __init__(self, initial_rate=0.0001, max_iterations=1000, decreasing_rate=True):
+    def __init__(self, initial_rate, decreasing_rate, X_normalized):
         self.initial_rate = initial_rate
-        self.max_iterations = max_iterations
         self.decreasing_rate = decreasing_rate
-        self.X_means = None
-        self.W = None
-        self.X_normalized = None
+        self.X_normalized = X_normalized
+        self.W = np.random.rand(X_normalized.shape[1])
 
-    def fit(self, X):
-        # Full normalization (centering and scaling)
-        self.X_mean = np.mean(X, axis=0)
-        self.X_std = np.std(X, axis=0)
-        self.X_normalized = (X - self.X_mean) / self.X_std
-
-        # Initialize the weight vector "W" with random values
-        self.W = np.random.rand(X.shape[1])
-        # Apply Oja's rule to find the first principal component
-        for iteration in range(self.max_iterations):
+    def fit(self, epochs):
+        for epoch in range(epochs):
             if self.decreasing_rate:
-                learning_rate = self.initial_rate * np.exp(-self.initial_rate * iteration)
+                learning_rate = self.initial_rate * np.exp(-self.initial_rate * epoch)
             else:
                 learning_rate = self.initial_rate
             for x in self.X_normalized:
                 y = x.dot(self.W)
-                delta_W = learning_rate * y * (x - y * self.W)  # Update rule for Oja's rule
+                delta_W = learning_rate * y * (x - y * self.W)
                 self.W += delta_W
 
     def get_first_principal_component(self):
         return self.W
-
-
-if __name__ == "__main__":
-    # Example usage:
-    # Create an instance of the OjaPCA class
-    oja_pca = OjaPCA()
-
-    # Load or generate your dataset as matrix "X"
-    entry = pd.read_csv("data/europe.csv").values
-    countries = entry[:, 0]
-    data = entry[:, 1:].astype(float)
-    X = np.array(data)
-
-    # Fit the OjaPCA model to your data
-    oja_pca.fit(X)
-
-    # Get the first principal component
-    first_principal_component = oja_pca.get_first_principal_component()
-
-    print("First Principal Component (Vector W):", first_principal_component)
